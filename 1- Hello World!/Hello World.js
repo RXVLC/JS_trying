6c30e6de3bca98226cb9183255af4a68a9564b5d
RXVLC <rafanavarrete115@gmail.com>
2023-09-08 01:51:39 +0200

    Fin por hoy, muy contento con los results

---
+++ 1- Hello World!/Hello World.js
@@ -0,0 +1,8 @@
+var tt = console.log("Hola pepe")
+
+if(tt == "Hola pepe"){
+    console.log("sii lo dijiste bien");
+}
+else{
+    console.log("repite");
+}
---
+++ 1- Hello World!/contador.js
@@ -0,0 +1,12 @@
+let contador;
+
+// for (contador=1; contador<=10; contador++){
+//     console.log("vamos por el: " + contador);
+// }
+// console.log("finished");
+
+contador = "0";
+do {
+    console.log('Valor de contador:' + contador);
+    contador++; // igual que contador = contador + 1
+} while (contador!="papa");
